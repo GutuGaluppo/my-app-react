ed58a7f15dadb51f2f9a2182b67abcb752e754e9
Gutu Galuppo <galuppodev@gmail.com>
2018-05-06 00:24:53 +0200

    Add function to button

--- src/App.js
+++ src/App.js
@@ -3,33 +3,106 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      value: 0,
+    };
+  }
+
+  /*handlePress9 = () => {
+    this.setState({
+      value: (this.state.value * 10) + 9,
+    });
+  };*/
+
+  handlePress = n => () => {
+    this.setState({
+      value: (this.state.value * 10) + n,
+    });
+  };
+
   render() {
     return (
       <div className="App">
-        GUTU
-        <div className="line">
-          <div className="flex-item">1</div>
-          <div className="flex-item">2</div>
-          <div className="flex-item">3</div>
-          <div className="flex-item">/</div>
+        CALCULATOR
+        <div className='output'>{this.state.value}</div>
+        <div className="line simb">
+          <div className="flex-item">
+            <div className="btn silver">AC</div>
+          </div>
+          <div className="flex-item">
+            <div className="btn silver">±</div>
+          </div>
+          <div className="flex-item">
+            <div className="btn silver">%</div>
+          </div>
+          <div className="flex-item">
+            <div className="btn orange simb">÷</div>
+          </div>
         </div>
-        <div className="line">
-          <div className="flex-item">4</div>
-          <div className="flex-item">5</div>
-          <div className="flex-item">6</div>
-          <div className="flex-item">x</div>
+        <div className="line simb">
+          <div className="flex-item">
+            <button
+              className="btn"
+              onClick={this.handlePress(7)}
+            >7</button>
+          </div>
+          <div className="flex-item">
+            <button
+              className="btn"
+              onClick={this.handlePress(8)}
+            >8</button>
+          </div>
+          <div className="flex-item">
+            <button
+              className="btn"
+              onClick={this.handlePress9}
+            >9</button>
+          </div>
+          <div className="flex-item">
+            <div className="btn orange simb">x</div>
+          </div>
         </div>
-        <div className="line">
-          <div className="flex-item">7</div>
-          <div className="flex-item">8</div>
-          <div className="flex-item">9</div>
-          <div className="flex-item">-</div>
+        <div className="line simb">
+          <div className="flex-item">
+            <div className="btn">4</div>
+          </div>
+          <div className="flex-item">
+            <div className="btn">5</div>
+          </div>
+          <div className="flex-item">
+            <div className="btn">6</div>
+          </div>
+          <div className="flex-item">
+            <div className="btn orange simb">-</div>
+          </div>
         </div>
-        <div className="line">
-          <div className="flex-item">0</div>
-          <div className="flex-item">,</div>
-          <div className="flex-item">c</div>
-          <div className="flex-item">=</div>
+        <div className="line simb">
+          <div className="flex-item">
+            <div className="btn">1</div>
+          </div>
+          <div className="flex-item">
+            <div className="btn">2</div>
+          </div>
+          <div className="flex-item">
+            <div className="btn">3</div>
+          </div>
+          <div className="flex-item">
+            <div className="btn orange">+</div>
+          </div>
+        </div>
+        <div className="line simb">
+          <div className="flex-item zero">
+            <div className="btn">0</div>
+          </div>
+          <div className="flex-item">
+            <div className="btn">.</div>
+          </div>
+          <div className="flex-item">
+            <div className="btn orange simb">=</div>
+          </div>
         </div>
       </div>
     );
